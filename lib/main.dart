32045d2e8ccaa8e9013baffcd568bd70ce002b13
Lans12345 <olanalans12345@gmail.com>
2025-11-29 06:00:28 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:ar_sci/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'utils/theme.dart';
 import 'utils/constants.dart';
 import 'routes/app_routes.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(
+    name: 'powernotify-64b1d',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // Set preferred orientations
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
