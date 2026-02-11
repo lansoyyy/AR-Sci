e616c3c9cb50a30ce1e98ceae289f471e31a360e
Lance Olana <101088979+lansoyyy@users.noreply.github.com>
2026-02-12 05:43:32 +0800

    reverted

--- lib/screens/student/ar_model_viewer_screen.dart
+++ lib/screens/student/ar_model_viewer_screen.dart
@@ -1,11 +1,7 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import '../../utils/colors.dart';
-import '../../utils/constants.dart';
 
 class ARModelViewerScreen extends StatefulWidget {
   final String title;
@@ -33,18 +29,13 @@ class _ARModelViewerScreenState extends State<ARModelViewerScreen> {
         title: Text(widget.title),
         backgroundColor: AppColors.studentPrimary,
       ),
-      body: Stack(
-        children: [
-          ModelViewer(
-            src: widget.modelAssetPath,
-            alt: widget.alt,
-            ar: true,
-            autoRotate: true,
-            cameraControls: true,
-            backgroundColor: const Color.fromARGB(0xFF, 0x00, 0x00, 0x00),
-            onWebViewCreated: (controller) {},
-          ),
-        ],
+      body: ModelViewer(
+        src: widget.modelAssetPath,
+        alt: widget.alt,
+        ar: widget.autoLaunchAr,
+        autoRotate: true,
+        cameraControls: true,
+        backgroundColor: const Color.fromARGB(0xFF, 0x00, 0x00, 0x00),
       ),
     );
   }
