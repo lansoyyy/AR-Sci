ffd1d3f4b61f64d91d89add96b7689971c1d240f
Lance Olana <101088979+lansoyyy@users.noreply.github.com>
2026-02-11 14:17:11 +0800

    fixed

--- lib/screens/student/ar_model_viewer_screen.dart
+++ lib/screens/student/ar_model_viewer_screen.dart
@@ -26,45 +26,6 @@ class ARModelViewerScreen extends StatefulWidget {
 }
 
 class _ARModelViewerScreenState extends State<ARModelViewerScreen> {
-  WebViewController? _controller;
-  Timer? _autoLaunchTimer;
-  bool _hasAttemptedAutoLaunch = false;
-
-  @override
-  void dispose() {
-    _autoLaunchTimer?.cancel();
-    super.dispose();
-  }
-
-  Future<void> _activateAr() async {
-    final controller = _controller;
-    if (controller == null) return;
-
-    try {
-      await controller.runJavaScript(
-        """
-        (function() {
-          const mv = document.querySelector('model-viewer');
-          if (mv && typeof mv.activateAR === 'function') {
-            mv.activateAR();
-          }
-        })();
-        """,
-      );
-    } catch (_) {}
-  }
-
-  void _scheduleAutoLaunch() {
-    if (!widget.autoLaunchAr) return;
-    if (_hasAttemptedAutoLaunch) return;
-
-    _hasAttemptedAutoLaunch = true;
-    _autoLaunchTimer?.cancel();
-    _autoLaunchTimer = Timer(const Duration(seconds: 2), () {
-      _activateAr();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,32 +42,7 @@ class _ARModelViewerScreenState extends State<ARModelViewerScreen> {
             autoRotate: true,
             cameraControls: true,
             backgroundColor: const Color.fromARGB(0xFF, 0x00, 0x00, 0x00),
-            onWebViewCreated: (controller) {
-              _controller = controller;
-              _scheduleAutoLaunch();
-            },
-          ),
-          Positioned(
-            left: AppConstants.paddingM,
-            right: AppConstants.paddingM,
-            bottom: AppConstants.paddingM,
-            child: SafeArea(
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton.icon(
-                  onPressed: _activateAr,
-                  icon: const Icon(Icons.view_in_ar),
-                  label: const Text('View in AR'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.studentPrimary,
-                    foregroundColor: AppColors.textWhite,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: AppConstants.paddingM,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            onWebViewCreated: (controller) {},
           ),
         ],
       ),
